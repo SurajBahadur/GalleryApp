a8a71f5486b2233ca401daf2c3e8d2a80526002a
Suraj Bahadur <suraj.bahadur2@gmail.com>
2025-06-05 13:29:47 +0530

    chore(di): initialize Hilt with Application class

---
+++ app/src/main/java/com/suraj/gallery/GalleryApplication.kt
@@ -0,0 +1,11 @@
+package com.suraj.gallery
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Suraj Bahadur on 6/5/2025.
+ */
+@HiltAndroidApp
+class GalleryApplication : Application() {
+}
