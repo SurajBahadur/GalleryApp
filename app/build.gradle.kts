32d5405cf0a9936d848650e50fc82d0fd20ddeb2
Suraj Bahadur <suraj.bahadur2@gmail.com>
2025-06-05 13:24:47 +0530

    chore(deps): add required libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,28 +1,35 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
 }
 
 android {
-    namespace = "com.suraj.gallery"
+    namespace = "com.gallery"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.suraj.gallery"
-        minSdk = 24
+        applicationId = "com.gallery"
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
+
+
+
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -33,6 +40,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +52,66 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //Architectural Components
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android.v173)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // RecyclerView
+    implementation(libs.androidx.recyclerview)
+
+    // SwipeRefreshLayout
+    implementation(libs.androidx.swiperefreshlayout)
+
+    // Image Loading
+    implementation(libs.glide)
+
+
+    /*//Moshi
+    implementation(libs.converter.moshi)
+    implementation(libs.moshi.kotlin)*/
+
+    ksp(libs.squareup.moshi.kotlin.codegen)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.common)
+
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation(libs.androidx.recyclerview)
+    testImplementation(kotlin("test"))
+
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.core.testing)
+    androidTestImplementation(libs.androidx.junit)
+
+    // Coroutine test utilities
+    testImplementation(libs.kotlinx.coroutines.test)
+    // Mockito for Kotlin
+    testImplementation(libs.mockito.kotlin)
+
+    // Turbine for testing StateFlow
+    testImplementation(libs.turbine)
+
+    androidTestImplementation(libs.androidx.junit.v115)
+    androidTestImplementation(libs.androidx.runner)
+    androidTestImplementation(libs.androidx.rules)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.runner)
+}
+kapt {
+    correctErrorTypes = true
 }
